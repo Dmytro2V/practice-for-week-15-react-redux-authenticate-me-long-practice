438b49f7c0f58dcee6d41ada25b527bed3ef856b
Dmytro2V <op2vin@gmail.com>
2023-04-11 10:13:43 +0300

    settle frontend, phase2-3

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,6 +1,30 @@
+// frontend/src/App.js
+import React, {useEffect} from 'react';
+import { useDispatch } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
+import LoginFormPage from './components/LoginFormPage'
+import {restoreUser} from './store/session'
 function App() {
+  const dispatch=useDispatch()
+  // const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(restoreUser()); //.then(() => setIsLoaded(true));
+  },[dispatch])
   return (
-    <h1>Hello from App</h1>
+    <> {/*isLoaded &&*/}
+    
+      <h4>(Hello from App)</h4>
+      <Switch>
+      <Route exact path="/">
+        <h3>Home</h3>
+      </Route>  
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
+    </>
+
+
   );
 }
 
